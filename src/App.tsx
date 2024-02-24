5dec4108c984b75db6b05c8a3ca87945898c9428
Dolev Klefner <dolevkle@gmail.com>
2024-02-24 14:41:21 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+// import { useState } from "react"
+// import ListGroup from "./ListGroup"
+// import Alert from "./Alert"
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
-import { NextUIProvider } from '@nextui-org/react'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+// function App() {
+//   const [cities, setCities] = useState(["London", "New York", "Paris", "Tokyo", "Delhi", "Dubai"])
+//   const [cars, setCars] = useState(["BMW", "Mercedes", "Audi", "Lamborghini", "Ferrari", "Porsche"])
+//   const [displayAleret, setDisplayAlert] = useState(false)
 
-// Create a new router instance
-const router = createRouter({ routeTree })
+//   const onDeleteCity = (index: number) => {
+//     console.log("Deleting city at index: " + index)
+//     const tmp = cities.filter((city, i) => i !== index)
+//     setCities(tmp)
+//   }
+
+//   const onDeleteCar = (index: number) => {
+//     console.log("Deleting car at index: " + index)
+//     setCars(cars.filter((car, i) => i !== index))
+//   }
+
+//   const openAlert = () => {
+//     setDisplayAlert(true)
+//   }
+
+//   const onDismiss = () => {
+//     setDisplayAlert(false)
+//   }
+//   return (
+//     <div className="p-2">
+//       {displayAleret && <Alert onDismiss={onDismiss}>This is an Alert!!!</Alert>}
+//       <h1>Cities!!</h1>
+//       <ListGroup items={cities} onDeleteItem={onDeleteCity} />
+
+//       <h1>Cars!!</h1>
+//       <ListGroup items={cars} onDeleteItem={onDeleteCar} />
+//       <div className="mx-auto pt-2 d-flex" style={{ width: "300px" }}>
+//         <button type="button"
+//           className="btn btn-primary flex-fill"
+//           onClick={openAlert}>
+//           Open alert
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
