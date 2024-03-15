bc1c95389ffd70e0e0efd9f46e67b64f58990264
Dolev Klefner <dolevkle@gmail.com>
2024-03-15 17:35:21 +0200

    [WIP] - added edit post only integration with backend remains

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import {
+  Button,
   Card,
   CardBody,
   CardHeader,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
   Image,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
   useDisclosure,
 } from "@nextui-org/react";
 import FullPost from "./FullPost";
+import EditPost from "./EditPost";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 export type Post = {
   title: string;
@@ -21,7 +32,19 @@ interface Props {
 }
 export default function Posts({ posts }: Props) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const {
+    isOpen: isEditOpen,
+    onOpen: onEditOpen,
+    onOpenChange: onEditOpenChange,
+  } = useDisclosure();
   const [selectedPost, setSelectedPost] = useState(posts[0]);
+
+  const [selectedKeys, setSelectedKeys] = useState(new Set([]));
+
+  const selectedValue = useMemo(
+    () => Array.from(selectedKeys).join(", "),
+    [selectedKeys]
+  );
 
   const handlePostPress = (post: Post) => {
     setSelectedPost(post);
@@ -36,10 +59,20 @@ export default function Posts({ posts }: Props) {
           isPressable
           onPress={() => handlePostPress(post)}
         >
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <p className="text-tiny uppercase font-bold">{post.title}</p>
-            <small className="text-default-500">{post.header}</small>
-            <h4 className="font-bold text-large">{post.subtitle}</h4>
+          <CardHeader className="pb-0 pt-2 px-4 justify-between">
+            <div className="flex flex-col items-start">
+              <p className="text-tiny uppercase font-bold">{post.title}</p>
+              <small className="text-default-500">{post.header}</small>
+              <h4 className="font-bold text-large">{post.subtitle}</h4>
+            </div>
+            <Popover placement="bottom">
+              <PopoverTrigger>
+                <FontAwesomeIcon icon={faEllipsis} className="self-start" />
+              </PopoverTrigger>
+              <PopoverContent className="p-0">
+               <Button onClick={onEditOpen} variant="light" fullWidth>Edit Post</Button>
+              </PopoverContent>
+            </Popover>
           </CardHeader>
           <CardBody className="overflow-visible py-2">
             <Image
@@ -55,6 +88,11 @@ export default function Posts({ posts }: Props) {
       <FullPost
         isOpen={isOpen}
         onOpenChange={onOpenChange}
+        post={selectedPost}
+      />
+      <EditPost
+        isOpen={isEditOpen}
+        onOpenChange={onEditOpenChange}
         post={selectedPost}
       />
     </>
